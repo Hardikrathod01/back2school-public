41fa6f9c5462da25d1dfd80183267f8677aa7135
hardikrathod01 <rathodhardik424@gmail.com>
2020-09-07 17:21:18 +0200

    fixed calendar module issue

--- src/app/components/student/student-calendar/event-utils.ts
+++ src/app/components/student/student-calendar/event-utils.ts
@@ -8,13 +8,16 @@ export const INITIAL_EVENTS: EventInput[] = [
     id: createEventId(),
     title: 'All-day event',
     start: TODAY_STR,
-    end: TODAY_STR
+    end: TODAY_STR,
+    description: 'BioChemistry'
   },
   {
     id: createEventId(),
     title: 'Timed event',
     start: TODAY_STR + 'T12:00:00',
-    end: TODAY_STR + 'T13:00:00'
+    end: TODAY_STR + 'T13:00:00',
+    description: 'BioChemistry'
+    
   }
 ];
 
--- src/app/components/student/student-calendar/student-calendar.component.ts
+++ src/app/components/student/student-calendar/student-calendar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
-import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/angular';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi, FullCalendarComponent } from '@fullcalendar/angular';
 import { INITIAL_EVENTS, createEventId } from './event-utils';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { MatDialogRef} from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import * as moment from 'moment';
 import { ThemePalette } from '@angular/material/core';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-student-calendar',
@@ -34,38 +35,17 @@ export class StudentCalendarComponent implements OnInit {
   hide = true;
   public eventForm = new FormGroup({
     eventName: new FormControl(),
-    eventDetail: new FormControl()
+    eventDetail: new FormControl(),
+    startDate: new FormControl(new Date(2020, 9, 4, 5, 6)),
+    endDate: new FormControl(new Date(2020, 9, 4, 5, 6)),
   });
 
-  public startDate = new FormControl(new Date(2020,9,4,5,6));
-  public endDate = new FormControl(new Date(2020,9,4,5,6));
+
   public selectInfo: DateSelectArg;
   eventName: string;
   eventDetail: String;
-  private  dialogRef: MatDialogRef<TemplateRef<any>>;
-  calendarVisible = true;
-  calendarOptions: CalendarOptions = {
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    initialView: 'dayGridMonth',
-    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true,
-    select: this.handleDateSelect.bind(this),
-    eventClick: this.handleEventClick.bind(this),
-    eventsSet: this.handleEvents.bind(this)
-    /* you can update a remote database when these fire:
-    eventAdd:
-    eventChange:
-    eventRemove:
-    */
-  }
+  private dialogRef: MatDialogRef<TemplateRef<any>>;
+  calendarOptions: CalendarOptions;
 
   currentEvents: EventApi[] = [];
 
@@ -80,11 +60,7 @@ export class StudentCalendarComponent implements OnInit {
     this.dialogRef = this.dialog.open(this.myTemplate, dialogConfig);
   }
 
-  handleEventClick(clickInfo: EventClickArg) {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove();
-    }
-  }
+
 
   handleEvents(events: EventApi[]) {
     this.currentEvents = events;
@@ -98,6 +74,21 @@ export class StudentCalendarComponent implements OnInit {
     this.dialogRef.close();
   }
 
+  handleEventClick(clickInfo: EventClickArg) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.minHeight = '300px';
+    dialogConfig.minWidth = '400px';
+    this.eventForm.controls['eventName'].setValue(clickInfo.event.title);
+    this.eventForm.controls['startDate'].setValue(clickInfo.event.start);
+    this.eventForm.controls['endDate'].setValue(clickInfo.event.end);
+    this.eventForm.controls['eventDetail'].setValue(clickInfo.event.extendedProps.description);
+    this.dialogRef = this.dialog.open(this.myTemplate, dialogConfig);
+
+  }
+
   save() {
     this.eventName = this.eventForm.get('eventName').value;
     this.eventDetail = this.eventForm.get('eventDetail').value;
@@ -105,21 +96,46 @@ export class StudentCalendarComponent implements OnInit {
 
     calendarApi.unselect(); // clear date selection
 
-    
-      calendarApi.addEvent({
-        id: createEventId(),
-        title: this.eventName,
-        start: this.startDate.value,
-        end: this.endDate.value,
-        detail: this.eventDetail
-      });
-   
+    calendarApi.addEvent({
+      id: createEventId(),
+      title: this.eventName,
+      start: this.eventForm.get('startDate').value,
+      end: this.eventForm.get('endDate').value,
+      description: this.eventDetail
+    });
+
     this.dialogRef.close();
   }
 
   ngOnInit(): void {
   }
 
-  
+  ngAfterViewInit() {
+    this.calendarOptions = {
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      },
+      initialView: 'dayGridMonth',
+      events: INITIAL_EVENTS,
+      weekends: true,
+      contentHeight: "auto",
+      editable: true,
+      selectable: true,
+      selectMirror: true,
+      dayMaxEvents: true,
+      select: this.handleDateSelect.bind(this),
+      eventClick: this.handleEventClick.bind(this),
+      eventsSet: this.handleEvents.bind(this),
+
+      /* you can update a remote database when these fire:
+      eventAdd:
+      eventChange:
+      eventRemove:
+      */
+    }
+  }
+
   constructor(public dialog: MatDialog) { }
 }
